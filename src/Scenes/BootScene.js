f014dd74f50a0cb06f74768076de9f2c9e6d107c
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-08-01 21:25:52 +0530

    Add BootScene code

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -6,8 +6,10 @@ export default class BootScene extends Phaser.Scene {
   }
  
   preload () {
+    this.load.image('game-logo', '../src/assets/game-logo.png');
   }
  
   create () {
+    this.scene.start('Preloader');
   }
 };
