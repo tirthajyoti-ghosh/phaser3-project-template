49bde07ee7b1ffc2838e3e33fa17606187e67165
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-08-02 00:04:50 +0530

    Add asets and pre-loader scene

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -6,7 +6,7 @@ export default class BootScene extends Phaser.Scene {
   }
  
   preload () {
-    this.load.image('game-logo', '../src/assets/game-logo.png');
+    this.load.image('game-logo', '../src/assets/zenva_logo.png');
   }
  
   create () {
